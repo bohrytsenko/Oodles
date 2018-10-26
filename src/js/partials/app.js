d2904e84740b84310f7da01a18a465f9646d57d2
bohrytsenko <hrytsenkobohdan@gmail.com>
2018-10-26 12:33:36 +0200

    responsive

--- src/js/partials/app.js
+++ src/js/partials/app.js
@@ -1,8 +1,17 @@
 $( document ).ready(function() {
 
-    // Test
+    // Navigation
 
-    console.log('test');
+
+    $('.header_wrap__icon a').on('click', function () {
+        $('.header__over').toggleClass('header_over_active');
+        $('.header__navigation').toggleClass('header__fixed');
+    });
+
+    $('.header__over').on('click', function () {
+        $(this).removeClass('header_over_active');
+        $('.header__navigation').removeClass('header__fixed');
+    });
 });
 
 
